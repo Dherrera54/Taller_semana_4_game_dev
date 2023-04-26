f2e805a008e57128093351d74af81b0ccf36d5b5
Dario_herrera <d.herrerag@uniandes.edu.co>
2023-04-25 20:16:03 -0500

    initial commit base project

---
+++ src/engine/service_locator.py
@@ -0,0 +1,5 @@
+from src.engine.services.images_service import ImagesService
+from src.engine.services.sounds_service import SoundsService
+class ServiceLocator:
+    images_services = ImagesService()
+    sounds_service = SoundsService()
